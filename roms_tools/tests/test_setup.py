3564c26fdfd2488ccfc8880a17189b853f5739d0
TomNicholas <tom@cworthy.org>
2023-11-17 16:05:19 -0500

    add tests file

---
+++ roms_tools/tests/test_setup.py
@@ -0,0 +1,23 @@
+import pytest
+
+from roms_tools.setup import Grid
+
+
+class TestCreateGrid:
+    def test_simple(self):
+        ...
+
+    def test_raise_if_crossing_dateline(self):
+        with pytest.raises(ValueError, match="cannot cross Greenwich Meridian"):
+            # test grid centered over London
+            Grid(nx=3, ny=3, size_x=100, size_y=100, center_lon=0, center_lat=51.5)
+
+        # test Iceland grid which is rotated specifically to avoid Greenwich Meridian
+        grid = Grid(nx=3, ny=3, size_x=1800, size_y=2400, center_lon=-21, center_lat=61, rot=20)
+        assert isinstance(grid, Grid)
+
+
+class TestGridFromFile:
+    def test_roundtrip(self):
+        """Test that creating a grid, saving it to file, and re-opening it is the same as just creating it."""
+        ...

31ed14fb671b77edee0bf28dfc7067e093eecc5a
TomNicholas <tom@cworthy.org>
2023-11-20 11:15:34 -0500

    some linting fixes

--- roms_tools/tests/test_setup.py
+++ roms_tools/tests/test_setup.py
@@ -13,7 +13,9 @@ class TestCreateGrid:
             Grid(nx=3, ny=3, size_x=100, size_y=100, center_lon=0, center_lat=51.5)
 
         # test Iceland grid which is rotated specifically to avoid Greenwich Meridian
-        grid = Grid(nx=3, ny=3, size_x=1800, size_y=2400, center_lon=-21, center_lat=61, rot=20)
+        grid = Grid(
+            nx=3, ny=3, size_x=1800, size_y=2400, center_lon=-21, center_lat=61, rot=20
+        )
         assert isinstance(grid, Grid)
 
 

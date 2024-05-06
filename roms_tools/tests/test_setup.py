9cee8a7195e07740df840065cfaa07ed5b884aa0
Nora Loose <NoraLoose@users.noreply.github.com>
2024-05-06 11:33:18 -0600

    Add more variables to grid regression test (#16)
    * Add more variables to grid regression test
    
    * Add comment

--- roms_tools/tests/test_setup.py
+++ roms_tools/tests/test_setup.py
@@ -1,18 +1,17 @@
 import pytest
 import numpy as np
 import numpy.testing as npt
-
 from roms_tools import Grid
 
 
 class TestCreateGrid:
     def test_simple_regression(self):
-        grid = Grid(nx=1, ny=1, size_x=100, size_y=100, center_lon=-20, center_lat=0)
+        grid = Grid(nx=1, ny=1, size_x=100, size_y=100, center_lon=-20, center_lat=0, rot=0)
 
         expected_lat = np.array(
             [
                 [1.79855429e00, 1.79855429e00, 1.79855429e00],
-                [1.72818690e-14, 1.70960078e-14, 1.70960078e-14],
+                [0.0, 0.0, 0.0],
                 [-1.79855429e00, -1.79855429e00, -1.79855429e00],
             ]
         )
@@ -23,9 +22,42 @@ class TestCreateGrid:
                 [339.10072286, 340.0, 340.89927714],
             ]
         )
+        expected_angle = np.array(
+            [
+                [0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0],
+            ]
+        )
+        expected_f0 = np.array(
+            [
+                [4.56484162e-06,  4.56484162e-06,  4.56484162e-06],
+                [0.0, 0.0, 0.0],
+                [-4.56484162e-06, -4.56484162e-06, -4.56484162e-06],
+            ]
+        )
+        expected_pm = np.array(
+            [
+                [5.0e-06,  5.0e-06, 5.0e-06],
+                [5.0e-06,  5.0e-06, 5.0e-06],
+                [5.0e-06,  5.0e-06, 5.0e-06],
+            ]
+        )
+        expected_pn = np.array(
+            [
+                [1.0004929e-05, 1.0004929e-05, 1.0004929e-05],
+                [1.0e-05,  1.0e-05, 1.0e-05],
+                [1.0004929e-05, 1.0004929e-05, 1.0004929e-05],
+            ]
+        )
 
+        # TODO: adapt tolerances according to order of magnitude of respective fields
         npt.assert_allclose(grid.ds["lat_rho"], expected_lat, atol=1e-8)
         npt.assert_allclose(grid.ds["lon_rho"], expected_lon, atol=1e-8)
+        npt.assert_allclose(grid.ds["angle"], expected_angle, atol=1e-8)
+        npt.assert_allclose(grid.ds["f0"], expected_f0, atol=1e-8)
+        npt.assert_allclose(grid.ds["pm"], expected_pm, atol=1e-8)
+        npt.assert_allclose(grid.ds["pn"], expected_pn, atol=1e-8)
 
     def test_raise_if_crossing_dateline(self):
         with pytest.raises(ValueError, match="cannot cross Greenwich Meridian"):

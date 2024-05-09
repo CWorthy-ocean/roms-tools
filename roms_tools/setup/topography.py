e36f3280dfeb5a45d2afc34719e10c6ee4a296a4
Nora Loose <nora.loose@gmail.com>
2024-05-09 13:20:11 -0700

    Smooth topography

--- roms_tools/setup/topography.py
+++ roms_tools/setup/topography.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import numpy as np
+import gcm_filters
 from scipy.interpolate import RegularGridInterpolator
 from roms_tools.setup.datasets import fetch_topo
 
@@ -20,6 +21,11 @@ def _add_topography_and_mask(ds, topography_source) -> xr.Dataset:
     mask.attrs = {"long_name": "Mask at rho-points", "units": "land/water (0/1)"}
     ds["mask_rho"] = mask
 
+    # smooth topography to avoid grid scale instabilities
+    ds["hsmooth"] = _smooth_topography(ds["hraw"], ds["mask_rho"])
+
+    # fill in lakes with land
+
     return ds
 
 def _make_raw_topography(lon, lat, topography_source) -> np.ndarray:
@@ -35,11 +41,11 @@ def _make_raw_topography(lon, lat, topography_source) -> np.ndarray:
         topo_lon = topo_ds["topo_lon"].copy()
         # Modify longitude values where necessary
         topo_lon = xr.where(topo_lon < 0, topo_lon + 360, topo_lon)
-        # Create a new longitude coordinate with modified values
         topo_lon_minus360 = topo_lon - 360
+        topo_lon_plus360 = topo_lon + 360
         # Concatenate along the longitude axis
-        topo_lon_concatenated = xr.concat([topo_lon_minus360, topo_lon], dim="lon")
-        topo_concatenated = xr.concat([topo_ds["topo"], topo_ds["topo"]], dim="lon")
+        topo_lon_concatenated = xr.concat([topo_lon_minus360, topo_lon, topo_lon_plus360], dim="lon")
+        topo_concatenated = xr.concat([topo_ds["topo"], topo_ds["topo"], topo_ds["topo"]], dim="lon")
 
         interp = RegularGridInterpolator((topo_ds["topo_lat"].values, topo_lon_concatenated.values), topo_concatenated.values)
 
@@ -47,3 +53,22 @@ def _make_raw_topography(lon, lat, topography_source) -> np.ndarray:
     hraw = interp((lat, lon))
 
     return hraw
+
+def _smooth_topography(hraw, wet_mask) -> xr.DataArray:
+
+    # we choose a Gaussian filter kernel with filter scale 8 (corresponding to a Gaussian with standard deviation 8/sqrt(12),
+    # see https://gcm-filters.readthedocs.io/en/latest/theory.html#filter-scale-and-shape)
+    filter = gcm_filters.Filter(
+        filter_scale=8,
+        dx_min=1,
+        filter_shape=gcm_filters.FilterShape.GAUSSIAN,
+        grid_type=gcm_filters.GridType.REGULAR_WITH_LAND,
+        grid_vars={'wet_mask': wet_mask}
+    
+    )
+    hsmooth = filter.apply(hraw, dims=["eta_rho", "xi_rho"])
+
+    return hsmooth
+
+
+

8f1b17b0ff43d0a88dca0623ea08ee792f8171be
Nora Loose <nora.loose@gmail.com>
2024-05-14 10:55:17 -0700

    Local topography smoothing and some renaming

--- roms_tools/setup/topography.py
+++ roms_tools/setup/topography.py
@@ -4,34 +4,41 @@ import gcm_filters
 from scipy.interpolate import RegularGridInterpolator
 from scipy.ndimage import label
 from roms_tools.setup.datasets import fetch_topo
+import warnings
 
-def _add_topography_and_mask(ds, topography_source, smooth_factor, hmin, rmax) -> xr.Dataset:
+def _add_topography_and_mask(ds, topography_source, smooth_factor, hmin, rmax, iter_max) -> xr.Dataset:
 
     lon = ds.lon_rho.values
     lat = ds.lat_rho.values
 
     hraw = _make_raw_topography(lon, lat, topography_source)
-    ds["hraw"] = xr.Variable(
-        data=hraw,
-        dims=["eta_rho", "xi_rho"],
-        attrs={"long_name": "Working bathymetry at rho-points", "units": "meter"},
-    )
+    hraw = xr.Variable(data=hraw, dims=["eta_rho", "xi_rho"])
 
-    # Mask is obtained by finding locations where water depth is positive
-    mask = xr.where(ds["hraw"] > 0, 1, 0)
-    mask.attrs = {"long_name": "Mask at rho-points", "units": "land/water (0/1)"}
-    ds["mask_rho"] = mask
+    # Mask is obtained by finding locations where ocean depth is positive 
+    mask = xr.where(hraw > 0, 1, 0)
 
     # smooth topography globally with Gaussian kernel to avoid grid scale instabilities
-    ds["hsmooth"] = _smooth_topography_globally(ds["hraw"], ds["mask_rho"], smooth_factor)
+    ds["hraw"] = _smooth_topography_globally(hraw, mask, smooth_factor)
+    ds["hraw"].attrs = {
+        "long_name": "Working bathymetry at rho-points", 
+        "source": f"Raw bathymetry from {topography_source} (smoothing diameter {smooth_factor})",
+        "units": "meter",
+    }
 
     # fill enclosed basins with land
-    mask = _fill_enclosed_basins(ds["mask_rho"].copy().values)
-    ds["mask_rho_filled"] = xr.DataArray(mask, dims=("eta_rho", "xi_rho"))
-    ds["hsmooth"] = ds["hsmooth"] * ds["mask_rho_filled"]
+    mask = _fill_enclosed_basins(mask.values)
+    ds["mask_rho"] = xr.DataArray(mask, dims=("eta_rho", "xi_rho"))
+    ds["mask_rho"].attrs = {
+            "long_name": "Mask at rho-points", 
+            "units": "land/water (0/1)"
+    }
 
     # smooth topography locally to satisfy r < rmax
-    #ds["hfinal"] = _smooth_topography_locally(ds["hsmooth"], hmin, rmax)
+    ds["h"] = _smooth_topography_locally(ds["hraw"] * ds["mask_rho"], hmin, rmax, iter_max)
+    ds["h"].attrs = {
+        "long_name": "Final bathymetry at rho-points", 
+        "units": "meter",
+    }
 
     return ds
 
@@ -63,8 +70,8 @@ def _make_raw_topography(lon, lat, topography_source) -> np.ndarray:
 
 def _smooth_topography_globally(hraw, wet_mask, factor) -> xr.DataArray:
 
-    # we choose a Gaussian filter kernel (corresponding to a Gaussian with standard deviation factor/sqrt(12),
-    # see https://gcm-filters.readthedocs.io/en/latest/theory.html#filter-scale-and-shape)
+    # we choose a Gaussian filter kernel corresponding to a Gaussian with standard deviation factor/sqrt(12);
+    # this standard deviation matches the standard deviation of a boxcar kernel with total width equal to factor.
     filter = gcm_filters.Filter(
         filter_scale=factor,
         dx_min=1,
@@ -78,6 +85,9 @@ def _smooth_topography_globally(hraw, wet_mask, factor) -> xr.DataArray:
     return hsmooth
 
 def _fill_enclosed_basins(mask) -> np.ndarray:
+    """
+    Fills in enclosed basins with land
+    """
 
     # Label connected regions in the mask
     reg, nreg = label(mask)
@@ -97,6 +107,89 @@ def _fill_enclosed_basins(mask) -> np.ndarray:
 
     return mask
 
+def _smooth_topography_locally(h, hmin=5, rmax=0.2, iter_max=500):
+    """
+    Smoothes topography locally to satisfy r < rmax
+    """
+    # Compute rmax_log
+    if rmax > 0.0:
+        rmax_log = np.log((1.0 + rmax * 0.9) / (1.0 - rmax * 0.9))
+    else:
+        rmax_log = 0.0
+    
+    # Apply hmin threshold
+    h = xr.where(h < hmin, hmin, h)    
+
+    # We will smooth logarithmically
+    h_log = np.log(h / hmin)
+
+    cf1 = 1.0 / 6
+    cf2 = 0.25
+
+    for iter in range(iter_max):
+
+        # Compute gradients in domain interior
+        
+        # in eta-direction
+        cff = h_log.diff("eta_rho").isel(xi_rho=slice(1, -1))
+        cr = np.abs(cff)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")  # Ignore division by zero warning
+            Op1 = xr.where(cr < rmax_log, 0, 1.0 * cff * (1 - rmax_log / cr))
+
+        # in xi-direction
+        cff = h_log.diff("xi_rho").isel(eta_rho=slice(1, -1))
+        cr = np.abs(cff)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")  # Ignore division by zero warning
+            Op2 = xr.where(cr < rmax_log, 0, 1.0 * cff * (1 - rmax_log / cr))
+
+        # in diagonal direction
+        cff = (h_log - h_log.shift(eta_rho=1, xi_rho=1)).isel(eta_rho=slice(1, None), xi_rho=slice(1, None))
+        cr = np.abs(cff)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")  # Ignore division by zero warning
+            Op3 = xr.where(cr < rmax_log, 0, 1.0 * cff * (1 - rmax_log / cr))
+    
+        # in the other diagonal direction
+        cff = (h_log.shift(eta_rho=1) - h_log.shift(xi_rho=1)).isel(eta_rho=slice(1, None), xi_rho=slice(1, None))
+        cr = np.abs(cff)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")  # Ignore division by zero warning
+            Op4 = xr.where(cr < rmax_log, 0, 1.0 * cff * (1 - rmax_log / cr))
+
+        # Update h_log in domain interior
+        h_log[1:-1, 1:-1] += cf1 * (Op1[1:, :] - Op1[:-1, :] + Op2[:, 1:] - Op2[:, :-1]
+                                  + cf2 * (Op3[1:, 1:] - Op3[:-1, :-1] + Op4[:-1, 1:] - Op4[1:, :-1]))
+
+        # No gradient at the domain boundaries
+        h_log[0, :] = h_log[1, :]
+        h_log[-1, :] = h_log[-2, :]
+        h_log[:, 0] = h_log[:, 1]
+        h_log[:, -1] = h_log[:, -2]
+
+        # Update h
+        h = hmin * np.exp(h_log)
+        # Apply hmin threshold again
+        h = xr.where(h < hmin, hmin, h)    
+
+        # compute maximum slope parameter r
+        r_eta, r_xi = _compute_rfactor(h)
+        rmax0 = np.max([r_eta.max(), r_xi.max()])
+        if rmax0 < rmax:
+            break
+        if iter == iter_max - 1:
+            raise Warning(f'After final (iter_max = {iter_max}) iteration of local topography smoothing, rmax = {rmax0} is not below threshold {rmax}. Please increase iter_max.')
 
 
+    return h
 
+def _compute_rfactor(h):
+    """
+    Computes slope parameter (or r-factor) r = |Delta h| / 2h in both horizontal grid directions.
+    """
+    # compute r_{i-1/2} = |h_i - h_{i-1}| / (h_i + h_{i+1})
+    r_eta = np.abs(h.diff("eta_rho")) / (h + h.shift(eta_rho=1)).isel(eta_rho=slice(1, None))
+    r_xi = np.abs(h.diff("xi_rho")) / (h + h.shift(xi_rho=1)).isel(xi_rho=slice(1, None))
+    
+    return r_eta, r_xi

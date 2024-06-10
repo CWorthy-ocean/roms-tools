1a053466ab8e71135d8dd2394b9e43df2ec91c2e
Nora Loose <nora.loose@gmail.com>
2024-06-10 09:48:11 -0700

    More docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,15 +13,42 @@ author = 'roms-tools developers'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+        "sphinx.ext.autodoc",
+        "sphinx.ext.autosummary",
+        "sphinx.ext.napoleon",
+        "nbsphinx",
+        "sphinxcontrib.bibtex",
+        ]
+
+numpydoc_show_class_members = False
+napolean_google_docstring = True
+napolean_numpy_docstring = False
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
+#exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+napoleon_custom_sections = [("Returns", "params_style"),
+                            ("Sets Attributes", "params_style"),
+                            ("Required Parameter Sections", "params_style"),
+                            ("Assumptions", "notes_style"),
+                            ("Example Config YAML File", "example"),
+                            ]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
+#html_theme = 'alabaster'
 html_static_path = ['_static']
+
+bibtex_bibfiles = ['references.bib']
+bibtex_reference_style = "author_year"
+
+html_theme_options = {
+    "repository_url": "https://github.com/timothyas/xesn",
+    "use_repository_button": True,
+}
+

82cb382f3cf23c4a83c52130e5a264a487d8ec89
Nora Loose <nora.loose@gmail.com>
2024-07-16 12:45:44 -0600

    Add regression tests

--- roms_tools/tests/test_topography.py
+++ roms_tools/tests/test_topography.py
@@ -5,51 +5,74 @@ import numpy.testing as npt
 from scipy.ndimage import label
 
 
-class TestTopography:
-    def test_enclosed_regions(self):
-        """Test that there are only two connected regions, one dry and one wet."""
+def test_enclosed_regions():
+    """Test that there are only two connected regions, one dry and one wet."""
 
-        grid = Grid(
-            nx=100,
-            ny=100,
-            size_x=1800,
-            size_y=2400,
-            center_lon=30,
-            center_lat=61,
-            rot=20,
-        )
+    grid = Grid(
+        nx=100,
+        ny=100,
+        size_x=1800,
+        size_y=2400,
+        center_lon=30,
+        center_lat=61,
+        rot=20,
+    )
 
-        reg, nreg = label(grid.ds.mask_rho)
-        npt.assert_equal(nreg, 2)
+    reg, nreg = label(grid.ds.mask_rho)
+    npt.assert_equal(nreg, 2)
 
-    def test_rmax_criterion(self):
-        grid = Grid(
-            nx=100,
-            ny=100,
-            size_x=1800,
-            size_y=2400,
-            center_lon=30,
-            center_lat=61,
-            rot=20,
-            smooth_factor=4,
-            rmax=0.2,
-        )
-        r_eta, r_xi = _compute_rfactor(grid.ds.h)
-        rmax0 = np.max([r_eta.max(), r_xi.max()])
-        npt.assert_array_less(rmax0, grid.rmax)
 
-    def test_hmin_criterion(self):
-        grid = Grid(
-            nx=100,
-            ny=100,
-            size_x=1800,
-            size_y=2400,
-            center_lon=30,
-            center_lat=61,
-            rot=20,
-            smooth_factor=2,
-            rmax=0.2,
-            hmin=5,
-        )
+def test_rmax_criterion():
+    grid = Grid(
+        nx=100,
+        ny=100,
+        size_x=1800,
+        size_y=2400,
+        center_lon=30,
+        center_lat=61,
+        rot=20,
+        smooth_factor=4,
+        rmax=0.2,
+    )
+    r_eta, r_xi = _compute_rfactor(grid.ds.h)
+    rmax0 = np.max([r_eta.max(), r_xi.max()])
+    npt.assert_array_less(rmax0, grid.rmax)
 
-        assert np.less_equal(grid.hmin, grid.ds.h.min())
+
+def test_hmin_criterion():
+    grid = Grid(
+        nx=100,
+        ny=100,
+        size_x=1800,
+        size_y=2400,
+        center_lon=30,
+        center_lat=61,
+        rot=20,
+        smooth_factor=2,
+        rmax=0.2,
+        hmin=5,
+    )
+
+    assert np.less_equal(grid.hmin, grid.ds.h.min())
+
+
+def test_data_consistency():
+    """
+    Test that the topography generation remains consistent.
+    """
+
+    grid = Grid(
+        nx=3, ny=3, size_x=1500, size_y=1500, center_lon=235, center_lat=25, rot=-20
+    )
+
+    expected_h = np.array(
+        [
+            [4505.16995868, 4505.16995868, 4407.37986032, 4306.51226663, 4306.51226663],
+            [4505.16995868, 4505.16995868, 4407.37986032, 4306.51226663, 4306.51226663],
+            [4400.69482254, 4400.69482254, 3940.84931344, 3060.19573878, 3060.19573878],
+            [4234.97356606, 4234.97356606, 2880.90226836, 2067.46801754, 2067.46801754],
+            [4234.97356606, 4234.97356606, 2880.90226836, 2067.46801754, 2067.46801754],
+        ]
+    )
+
+    assert np.allclose(grid.ds["h"].values, expected_h)

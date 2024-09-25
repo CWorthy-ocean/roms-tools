d73db4030e49c7348a0806b8343e22b0b6cd427e
Nora Loose <NoraLoose@users.noreply.github.com>
2024-09-25 12:11:15 -0600

    AMG Reproducibility (#148)
    * Set random seed to ensure reproducibility with AMG
    
    * Overwrite test data with fixed random seed
    
    * Lower tolerance rtol again in validation tests
    
    * Include hash tests again
    
    * Include unit tests for lateral fill
    
    * Linting
    
    * Overwrite test data

--- roms_tools/tests/test_setup/test_initial_conditions.py
+++ roms_tools/tests/test_setup/test_initial_conditions.py
@@ -7,6 +7,7 @@ import textwrap
 from roms_tools.setup.download import download_test_data
 from roms_tools.setup.datasets import CESMBGCDataset
 from pathlib import Path
+from conftest import calculate_file_hash
 
 
 @pytest.mark.parametrize(
@@ -269,26 +270,25 @@ def test_roundtrip_yaml(initial_conditions, tmp_path, use_dask):
         filepath.unlink()
 
 
-# TODO: Solve PyAMG reproducibility issue and uncomment this test
-# def test_files_have_same_hash(initial_conditions, tmp_path, use_dask):
-#
-#    yaml_filepath = tmp_path / "test_yaml.yaml"
-#    filepath1 = tmp_path / "test1.nc"
-#    filepath2 = tmp_path / "test2.nc"
-#
-#    initial_conditions.to_yaml(yaml_filepath)
-#    initial_conditions.save(filepath1)
-#    ic_from_file = InitialConditions.from_yaml(yaml_filepath, use_dask)
-#    ic_from_file.save(filepath2)
-#
-#    hash1 = calculate_file_hash(filepath1)
-#    hash2 = calculate_file_hash(filepath2)
-#
-#    assert hash1 == hash2, f"Hashes do not match: {hash1} != {hash2}"
-#
-#    yaml_filepath.unlink()
-#    filepath1.unlink()
-#    filepath2.unlink()
+def test_files_have_same_hash(initial_conditions, tmp_path, use_dask):
+
+    yaml_filepath = tmp_path / "test_yaml.yaml"
+    filepath1 = tmp_path / "test1.nc"
+    filepath2 = tmp_path / "test2.nc"
+
+    initial_conditions.to_yaml(yaml_filepath)
+    initial_conditions.save(filepath1)
+    ic_from_file = InitialConditions.from_yaml(yaml_filepath, use_dask)
+    ic_from_file.save(filepath2)
+
+    hash1 = calculate_file_hash(filepath1)
+    hash2 = calculate_file_hash(filepath2)
+
+    assert hash1 == hash2, f"Hashes do not match: {hash1} != {hash2}"
+
+    yaml_filepath.unlink()
+    filepath1.unlink()
+    filepath2.unlink()
 
 
 def test_from_yaml_missing_initial_conditions(tmp_path, use_dask):

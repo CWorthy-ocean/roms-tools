0c28564828384c482cc5f42ad89eee802bcd6339
Nora Loose <nora.loose@gmail.com>
2024-09-25 11:23:47 -0600

    Linting

--- roms_tools/tests/test_setup/test_fill.py
+++ roms_tools/tests/test_setup/test_fill.py
@@ -4,34 +4,36 @@ from roms_tools.setup.download import download_test_data
 from roms_tools.setup.fill import LateralFill
 from datetime import datetime
 import numpy as np
-from scipy import sparse
+
 
 @pytest.fixture()
 def era5_data(request, use_dask):
     fname = download_test_data("ERA5_regional_test_data.nc")
     data = ERA5Dataset(
-        filename = fname,
+        filename=fname,
         start_time=datetime(2020, 1, 31),
         end_time=datetime(2020, 2, 2),
-        use_dask=use_dask
-            )
+        use_dask=use_dask,
+    )
     data.post_process()
 
     return data
+
 
 @pytest.fixture()
 def glorys_data(request, use_dask):
     fname = download_test_data("GLORYS_NA_2012.nc")
 
     data = GLORYSDataset(
-        filename = fname,
-        start_time=datetime(2012,1,1),
-        end_time=datetime(2013,1,1),
-        use_dask=use_dask
-            )
+        filename=fname,
+        start_time=datetime(2012, 1, 1),
+        end_time=datetime(2013, 1, 1),
+        use_dask=use_dask,
+    )
     data.post_process()
 
     return data
+
 
 @pytest.mark.parametrize(
     "data_fixture",
@@ -41,7 +43,7 @@ def glorys_data(request, use_dask):
     ],
 )
 def test_lateral_fill_reproducibility(data_fixture, request, use_dask):
-    
+
     data = request.getfixturevalue(data_fixture)
     lateral_fill0 = LateralFill(
         data.ds["mask"],
@@ -58,11 +60,10 @@ def test_lateral_fill_reproducibility(data_fixture, request, use_dask):
     for var in data.var_names:
 
         ds0[data.var_names[var]] = lateral_fill0.apply(
-                ds0[data.var_names[var]].astype(np.float64)
+            ds0[data.var_names[var]].astype(np.float64)
         )
         ds1[data.var_names[var]] = lateral_fill1.apply(
-                ds1[data.var_names[var]].astype(np.float64)
+            ds1[data.var_names[var]].astype(np.float64)
         )
 
     assert ds0.equals(ds1)
-

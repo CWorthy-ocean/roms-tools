b6f53f372445cfd4349834a1b329a230772ab607
Nora Loose <nora.loose@gmail.com>
2024-09-26 10:59:02 -0700

    Test that there are no NaNs present after lateral fill

--- roms_tools/tests/test_setup/test_fill.py
+++ roms_tools/tests/test_setup/test_fill.py
@@ -1,7 +1,8 @@
 import pytest
-from roms_tools.setup.datasets import GLORYSDataset, ERA5Dataset
+from roms_tools.setup.datasets import GLORYSDataset, ERA5Dataset, CESMBGCDataset, CESMBGCSurfaceForcingDataset, TPXODataset
 from roms_tools.setup.download import download_test_data
 from roms_tools.setup.fill import LateralFill
+from roms_tools.setup.utils import extrapolate_deepest_to_bottom
 from datetime import datetime
 import numpy as np
 
@@ -32,8 +33,84 @@ def glorys_data(request, use_dask):
     )
     data.post_process()
 
+    print(data.var_names)
+    # extrapolate deepest value to bottom so all levels can use the same surface mask
+    for var in data.var_names:
+        if var != "zeta":
+            data.ds[data.var_names[var]] = extrapolate_deepest_to_bottom(
+                data.ds[data.var_names[var]], data.dim_names["depth"]
+                )
+
     return data
 
+@pytest.fixture()
+def tpxo_data(request, use_dask):
+    fname = download_test_data("TPXO_regional_test_data.nc")
+
+    data = TPXODataset(
+        filename=fname,
+        use_dask=use_dask,
+    )
+    data.post_process()
+
+    return data
+
+@pytest.fixture()
+def cesm_bgc_data(request, use_dask):
+    fname = download_test_data("CESM_BGC_2012.nc")
+
+    data = CESMBGCDataset(
+        filename=fname,
+        start_time=datetime(2012, 1, 1),
+        end_time=datetime(2013, 1, 1),
+        climatology=False,
+        use_dask=use_dask,
+    )
+    data.post_process()
+    
+    # extrapolate deepest value to bottom so all levels can use the same surface mask
+    for var in data.var_names:
+        data.ds[data.var_names[var]] = extrapolate_deepest_to_bottom(
+            data.ds[data.var_names[var]], data.dim_names["depth"]
+            )
+
+    return data
+
+@pytest.fixture()
+def cesm_surface_bgc_data(request, use_dask):
+    fname = download_test_data("CESM_BGC_SURFACE_2012.nc")
+
+    data = CESMBGCSurfaceForcingDataset(
+        filename=fname,
+        start_time=datetime(2012, 1, 1),
+        end_time=datetime(2013, 1, 1),
+        climatology=True,
+        use_dask=use_dask,
+    )
+    data.post_process()
+
+    return data
+
+@pytest.mark.parametrize(
+    "data_fixture",
+    [
+        "era5_data",
+        "glorys_data",
+        "tpxo_data",
+        "cesm_bgc_data",
+        "cesm_surface_bgc_data"
+    ],
+)
+def test_lateral_fill_no_nans(data_fixture, request, use_dask):
+    data = request.getfixturevalue(data_fixture)
+    lateral_fill = LateralFill(
+        data.ds["mask"],
+        [data.dim_names["latitude"], data.dim_names["longitude"]],
+    )
+
+    for var in data.var_names:
+        filled = lateral_fill.apply(data.ds[data.var_names[var]].astype(np.float64))
+        assert not filled.isnull().any()
 
 @pytest.mark.parametrize(
     "data_fixture",

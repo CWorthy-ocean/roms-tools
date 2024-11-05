d6e85fc6a88985fd5e6064ed20bb1f9f4514e193
Nora Loose <nora.loose@gmail.com>
2024-11-05 14:01:34 -0800

    Temporal grouping is optional with default false

--- roms_tools/setup/surface_forcing.py
+++ roms_tools/setup/surface_forcing.py
@@ -502,24 +502,22 @@ class SurfaceForcing:
         )
 
     def save(
-        self, filepath: Union[str, Path], np_eta: int = None, np_xi: int = None
+            self, filepath: Union[str, Path], np_eta: int = None, np_xi: int = None, group : bool = False
     ) -> None:
-        """Save the surface forcing fields to netCDF4 files.
+        """Save the surface forcing fields to one or more netCDF4 files.
 
-        This method saves the dataset by grouping it into subsets based on the data frequency. The subsets are then written
-        to one or more netCDF4 files. The filenames of the output files reflect the temporal coverage of the data.
+        This method saves the dataset either as a single file or as multiple files depending on the partitioning and grouping options.
+        The dataset can be saved in two modes:
 
-        There are two modes of saving the dataset:
+        1. **Single File Mode (default)**:
+            - If both `np_eta` and `np_xi` are `None`, the entire dataset is saved as a single netCDF4 file.
+            - The file is named based on the `filepath`, with `.nc` automatically appended.
+        
+        2. **Partitioned Mode**:
+            - If either `np_eta` or `np_xi` is specified, the dataset is partitioned into spatial tiles along the `eta` and `xi` axes.
+            - Each tile is saved as a separate netCDF4 file, and filenames are modified with an index (e.g., `"filepath_YYYYMM.0.nc"`, `"filepath_YYYYMM.1.nc"`).
 
-          1. **Single File Mode (default)**:
-
-            If both `np_eta` and `np_xi` are `None`, the entire dataset, divided by temporal subsets, is saved as a single netCDF4 file
-            with the base filename specified by `filepath.nc`.
-
-          2. **Partitioned Mode**:
-
-            - If either `np_eta` or `np_xi` is specified, the dataset is divided into spatial tiles along the eta-axis and xi-axis.
-            - Each spatial tile is saved as a separate netCDF4 file.
+        Additionally, if `group` is set to `True`, the dataset is first grouped into temporal subsets, resulting in multiple grouped files before partitioning and saving.
 
         Parameters
         ----------
@@ -531,6 +529,8 @@ class SurfaceForcing:
             The number of partitions along the `eta` direction. If `None`, no spatial partitioning is performed.
         np_xi : int, optional
             The number of partitions along the `xi` direction. If `None`, no spatial partitioning is performed.
+        group: bool, optional
+            If `True`, groups the dataset into multiple files based on temporal data frequency. Defaults to `False`.
 
         Returns
         -------
@@ -544,8 +544,13 @@ class SurfaceForcing:
         # Remove ".nc" suffix if present
         if filepath.suffix == ".nc":
             filepath = filepath.with_suffix("")
+        
+        if group:
+            dataset_list, output_filenames = group_dataset(self.ds.load(), str(filepath))
+        else:
+            dataset_list = [self.ds.load()]
+            output_filenames = [str(filepath)]
 
-        dataset_list, output_filenames = group_dataset(self.ds.load(), str(filepath))
         saved_filenames = save_datasets(
             dataset_list, output_filenames, np_eta=np_eta, np_xi=np_xi
         )

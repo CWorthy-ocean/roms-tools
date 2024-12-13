e895ae8d9d5d6aedae07d8c55ef709f443f7216d
Nora Loose <nora.loose@gmail.com>
2024-12-13 13:32:56 -0800

    Linting

--- roms_tools/tests/test_setup/test_nesting.py
+++ roms_tools/tests/test_setup/test_nesting.py
@@ -6,24 +6,36 @@ from roms_tools import Grid, Nesting
 from roms_tools.setup.utils import get_boundary_coords
 from roms_tools.setup.nesting import interpolate_indices
 
+
 @pytest.fixture()
 def parent_grid():
-    return Grid(nx=5, ny=7, center_lon=-23, center_lat=61, rot=20, size_x=1800, size_y=2400)
+    return Grid(
+        nx=5, ny=7, center_lon=-23, center_lat=61, rot=20, size_x=1800, size_y=2400
+    )
+
 
 @pytest.fixture()
 def child_grid():
-    return Grid(nx=10, ny=10, center_lon=-23, center_lat=61, rot=-20, size_x=500, size_y=500)
+    return Grid(
+        nx=10, ny=10, center_lon=-23, center_lat=61, rot=-20, size_x=500, size_y=500
+    )
+
 
 @pytest.fixture()
 def parent_grid_that_straddles():
-    return Grid(nx=5, ny=7, center_lon=10, center_lat=61, rot=20, size_x=1800, size_y=2400)
+    return Grid(
+        nx=5, ny=7, center_lon=10, center_lat=61, rot=20, size_x=1800, size_y=2400
+    )
+
 
 @pytest.fixture()
 def child_grid_that_straddles():
-    return Grid(nx=10, ny=10, center_lon=10, center_lat=61, rot=-20, size_x=500, size_y=500)
+    return Grid(
+        nx=10, ny=10, center_lon=10, center_lat=61, rot=-20, size_x=500, size_y=500
+    )
+
 
 class TestMapChildBoundaries:
-        
     @pytest.mark.parametrize(
         "grid",
         [
@@ -35,7 +47,7 @@ class TestMapChildBoundaries:
         """Test that the boundaries of the same grid are mapped correctly."""
 
         grid = request.getfixturevalue(grid)
-        
+
         bdry_coords_dict = get_boundary_coords()
         location = "rho"
         for direction in ["south", "east", "north", "west"]:
@@ -48,16 +60,23 @@ class TestMapChildBoundaries:
                 i_eta, i_xi = interpolate_indices(grid.ds, lon, lat, mask)
 
             # Verify the warning message in the log
-            assert "Some boundary points of the child grid are very close to the boundary of the parent grid." in caplog.text
-            
+            assert (
+                "Some boundary points of the child grid are very close to the boundary of the parent grid."
+                in caplog.text
+            )
+
             if direction == "south":
                 expected_i_eta = -0.5 * xr.ones_like(grid.ds.xi_rho)
                 expected_i_xi = np.arange(-0.5, grid.ds.xi_rho[-1] + 0.5)
             elif direction == "east":
                 expected_i_eta = np.arange(-0.5, grid.ds.eta_rho[-1] + 0.5)
-                expected_i_xi = (grid.ds.xi_rho[-1] - 0.5) * xr.ones_like(grid.ds.eta_rho)
+                expected_i_xi = (grid.ds.xi_rho[-1] - 0.5) * xr.ones_like(
+                    grid.ds.eta_rho
+                )
             elif direction == "north":
-                expected_i_eta = (grid.ds.eta_rho[-1] - 0.5) * xr.ones_like(grid.ds.xi_rho)
+                expected_i_eta = (grid.ds.eta_rho[-1] - 0.5) * xr.ones_like(
+                    grid.ds.xi_rho
+                )
                 expected_i_xi = np.arange(-0.5, grid.ds.xi_rho[-1] + 0.5)
             elif direction == "west":
                 expected_i_eta = np.arange(-0.5, grid.ds.eta_rho[-1] + 0.5)
@@ -70,10 +89,12 @@ class TestMapChildBoundaries:
         "parent_grid_fixture, child_grid_fixture",
         [
             ("parent_grid", "child_grid"),
-            ("parent_grid_that_straddles", "child_grid_that_straddles")
+            ("parent_grid_that_straddles", "child_grid_that_straddles"),
         ],
     )
-    def test_indices_are_within_range_of_parent_grid(self, parent_grid_fixture, child_grid_fixture, request):
+    def test_indices_are_within_range_of_parent_grid(
+        self, parent_grid_fixture, child_grid_fixture, request
+    ):
 
         parent_grid = request.getfixturevalue(parent_grid_fixture)
         child_grid = request.getfixturevalue(child_grid_fixture)
@@ -86,7 +107,7 @@ class TestMapChildBoundaries:
                 mask = child_grid.ds[f"mask_{location}"].isel(**bdry_coords)
 
                 i_eta, i_xi = interpolate_indices(parent_grid.ds, lon, lat, mask)
-                
+
                 expected_i_eta_min = -0.5
                 expected_i_eta_max = parent_grid.ds.eta_rho[-1] - 0.5
                 expected_i_xi_min = -0.5
@@ -97,23 +118,24 @@ class TestMapChildBoundaries:
                 assert (i_xi >= expected_i_xi_min).all()
                 assert (i_xi <= expected_i_xi_max).all()
 
+
 class TestNesting:
     @pytest.mark.parametrize(
         "parent_grid_fixture, child_grid_fixture",
         [
             ("parent_grid", "child_grid_that_straddles"),
-            ("parent_grid_that_straddles", "child_grid")
+            ("parent_grid_that_straddles", "child_grid"),
         ],
     )
-    def test_error_if_child_grid_beyond_parent_grid(self, parent_grid_fixture, child_grid_fixture, request):
+    def test_error_if_child_grid_beyond_parent_grid(
+        self, parent_grid_fixture, child_grid_fixture, request
+    ):
         parent_grid = request.getfixturevalue(parent_grid_fixture)
         child_grid = request.getfixturevalue(child_grid_fixture)
-        
-        with pytest.raises(
-            ValueError, match="Some points are outside the grid."
-        ):
+
+        with pytest.raises(ValueError, match="Some points are outside the grid."):
             Nesting(parent_grid=parent_grid, child_grid=child_grid)
-            
+
 
 # Test map_child_boundaries_onto_parent_grid_indices
 # - if parent grid has no land along boundaries, update_indices doesn't do anything
@@ -121,12 +143,11 @@ class TestNesting:
 # - check that variable names, period and variable attributes are correctly set
 # - check that indices are monotonically increasing
 
-# Test modify_child_topography_and_mask 
-# - check that topography and mask are only modified near boundary 
+# Test modify_child_topography_and_mask
+# - check that topography and mask are only modified near boundary
 # - check that mask is not modified if there is no parent land along boundaries
 # - check that this does nothing if the parent_grid and child_grid coincide
 
 # Test compute_boundary_distance
 # - check that this is 1 at boundaries at 0 in interior if no land
 # - check that there are some 1 along boundaries if there is land along boundary
-

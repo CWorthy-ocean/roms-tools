352241c00295f3847328b5d5a731a793243a8655
Nora Loose <nora.loose@gmail.com>
2025-02-14 16:07:06 -0700

    Revert to xr.testing.assert_allclose

--- roms_tools/tests/test_setup/test_validation.py
+++ roms_tools/tests/test_setup/test_validation.py
@@ -72,4 +72,4 @@ def test_check_results(forcing_fixture, request):
     expected_forcing_ds = xr.open_zarr(fname, decode_timedelta=False)
     forcing = request.getfixturevalue(forcing_fixture)
 
-    xr.testing.assert_equal(forcing.ds, expected_forcing_ds)
+    xr.testing.assert_allclose(forcing.ds, expected_forcing_ds, rtol=1.0e-5, atol=1e-08)

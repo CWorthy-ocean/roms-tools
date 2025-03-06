526b87e3613be7c637c2948a74af5b6e9ec8362d
Nora Loose <nora.loose@gmail.com>
2025-03-06 08:25:52 -0800

    Correct docstrings

--- roms_tools/plot.py
+++ roms_tools/plot.py
@@ -19,8 +19,8 @@ def _plot(
 
     Parameters
     ----------
-    field : xarray.DataArray, optional
-        The field to plot. If None, only the grid is plotted.
+    field : xarray.DataArray
+        The field to plot.
     depth_contours : bool, optional
         If True, adds depth contours to the plot.
     c : str, optional

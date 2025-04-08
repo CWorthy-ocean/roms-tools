522579011b521bc1ceb35b62b1e7dc8e9ec47c38
Nora Loose <nora.loose@gmail.com>
2025-04-08 15:18:17 -0700

    Dictionary and plotting for CDR forcing

--- roms_tools/setup/cdr_forcing.py
+++ roms_tools/setup/cdr_forcing.py
@@ -1,16 +1,21 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Optional, List, Dict, Union
 import numpy as np
 import xarray as xr
+import cartopy.crs as ccrs
+import logging
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 from roms_tools import Grid
+from roms_tools.plot import _plot, _get_projection
+from roms_tools.setup.utils import gc_dist
 
 
 @dataclass(kw_only=True)
 class CDRPipeForcing:
     """Represents CDR pipe forcing data for ROMS, supporting both constant and time-
-    varying tracers and volumes.
+    varying tracer concentrations and volumes.
 
     Parameters
     ----------
@@ -22,6 +27,8 @@ class CDRPipeForcing:
         End time of the model simulation.
     model_reference_date : datetime, optional
         Reference date for converting absolute times to model-relative time. Defaults to Jan 1, 2000.
+    releases : dict, optional
+        A dictionary of existing CDR releases. Defaults to empty dictionary.
 
     Attributes
     ----------
@@ -33,23 +40,32 @@ class CDRPipeForcing:
     start_time: datetime
     end_time: datetime
     model_reference_date: datetime = datetime(2000, 1, 1)
+    releases: Optional[dict] = field(default_factory=dict)
 
     def __post_init__(self):
         if self.start_time >= self.end_time:
             raise ValueError("`start_time` must be earlier than `end_time`.")
 
-        self.ds = xr.Dataset(
-            {
-                "cdr_time": (["time"], np.empty(0)),
-                "cdr_volume": (["time", "ncdr"], np.empty((0, 0))),
-                # "cdr_tracer": (["time", "ntracers", "ncdr"], np.empty((0, 0))),
-            },
-            coords={
-                "time": (["time"], np.empty(0)),
-                "experiment_name": (["ncdr"], np.empty(0, dtype=str)),
-            },
-        )
-        self._add_global_metadata()
+        if not self.releases:
+
+            self.ds = xr.Dataset(
+                {
+                    "cdr_time": (["time"], np.empty(0)),
+                    "cdr_volume": (["time", "ncdr"], np.empty((0, 0))),
+                    # "cdr_tracer": (["time", "ntracers", "ncdr"], np.empty((0, 0))),
+                },
+                coords={
+                    "time": (["time"], np.empty(0)),
+                    "release_name": (["ncdr"], np.empty(0, dtype=str)),
+                },
+            )
+            self._add_global_metadata()
+        else:
+            for name, params in self.releases.items():
+                self._add_grid_indices_to_dict(
+                    name=name, lat=params["lat"], lon=params["lon"]
+                )
+                self._add_release_to_ds(name=name, **params)
 
     def _add_global_metadata(self):
         tracer_names = [
@@ -100,16 +116,16 @@ class CDRPipeForcing:
         release_start_time: Optional[datetime] = None,
         release_end_time: Optional[datetime] = None,
         times: Optional[List[datetime]] = None,
-        tracers: Optional[Dict[str, Union[float, List[float]]]] = None,
+        # tracer_conentrations: Optional[Dict[str, Union[float, List[float]]]] = None,
         volume: Union[float, List[float]] = 0.0,
         fill_values: Optional[str] = "auto_fill",
     ):
-        """Adds a CDR pipe release to the forcing dataset.
+        """Adds a CDR pipe release to the forcing dataset and dictionary.
 
         Parameters
         ----------
         name : str
-            Unique identifier for the experiment.
+            Unique identifier for the release.
         lat : float
             Latitude of the release location. Must be between -90 and 90.
         lon : float
@@ -121,17 +137,54 @@ class CDRPipeForcing:
         release_end_time : datetime, optional
             End time of the release. Required if `times` is `None`.
         times : List[datetime], optional
-            Explicit time points for time-varying tracers and volumes.
-        tracers : dict, optional
-            A dictionary of tracer names and their corresponding values, which can be constant or time-varying.
+            Explicit time points for time-varying tracer concentrations and volumes.
+        tracer_concentrations : dict, optional
+            A dictionary of tracer names and their corresponding concentration values, which can be constant or time-varying.
             Example formats:
-            - Constant tracers: {"temp": 20.0, "salt": 1.0, "ALK": 2000.0}
-            - Time-varying tracers: {"temp": [19.5, 20, 20, 20], "salt": [1.1, 2, 1, 1], "ALK": [2000.0, 2014.3, 2001.0, 2004.2]} (with `times` set to four corresponding datetime entries)
+            - Constant tracer concentrations: {"temp": 20.0, "salt": 1.0, "ALK": 2000.0}
+            - Time-varying tracer concentrations: {"temp": [19.5, 20, 20, 20], "salt": [1.1, 2, 1, 1], "ALK": [2000.0, 2014.3, 2001.0, 2004.2]} (with `times` set to four corresponding datetime entries)
         volume : float or list of float, optional
             Volume of release over time.
         fill_values : str, optional
-            Strategy for filling missing tracer values. Options: "auto_fill", "zero_fill".
+            Strategy for filling missing tracer concentration values. Options: "auto_fill", "zero_fill".
         """
+
+        self._add_release_to_dict(
+            name=name,
+            lat=lat,
+            lon=lon,
+            depth=depth,
+            release_start_time=release_start_time,
+            release_end_time=release_end_time,
+            times=times,
+            volume=volume,
+        )
+        self._add_release_to_ds(
+            name=name,
+            lat=lat,
+            lon=lon,
+            depth=depth,
+            release_start_time=release_start_time,
+            release_end_time=release_end_time,
+            times=times,
+            volume=volume,
+        )
+
+    def _add_release_to_ds(
+        self,
+        *,
+        name: str,
+        lat: float,
+        lon: float,
+        depth: float,
+        release_start_time: Optional[datetime] = None,
+        release_end_time: Optional[datetime] = None,
+        times: Optional[List[datetime]] = None,
+        tracer_concentrations: Optional[Dict[str, Union[float, List[float]]]] = None,
+        volume: Union[float, List[float]] = 0.0,
+    ):
+        """Adds a CDR pipe release to the forcing dataset."""
+
         self._input_checks(
             name,
             lat,
@@ -140,16 +193,13 @@ class CDRPipeForcing:
             release_start_time,
             release_end_time,
             times,
-            tracers,
+            tracer_concentrations,
             volume,
-            fill_values,
         )
 
         # Check that the name is unique
-        if name in self.ds["experiment_name"].values:
-            raise ValueError(
-                f"A release experiment with the name '{name}' already exists."
-            )
+        if name in self.ds["release_name"].values:
+            raise ValueError(f"A release with the name '{name}' already exists.")
 
         if release_start_time and release_end_time and not times:
             times = [release_start_time, release_end_time]
@@ -181,8 +231,8 @@ class CDRPipeForcing:
         ds["cdr_time"] = ("time", union_rel_time)
         ds["time"] = ("time", union_time)
 
-        experiment_names = np.concatenate([self.ds.experiment_name.values, [name]])
-        ds = ds.assign_coords({"experiment_name": (["ncdr"], experiment_names)})
+        release_names = np.concatenate([self.ds.release_name.values, [name]])
+        ds = ds.assign_coords({"release_name": (["ncdr"], release_names)})
         ds["cdr_volume"] = xr.zeros_like(ds.cdr_time * ds.ncdr)
         # ds["cdr_tracer"] = xr.zeros_like(ds.cdr_time * ds.ncdr)
 
@@ -210,6 +260,30 @@ class CDRPipeForcing:
         self.ds = ds
         self._add_global_metadata()
 
+    def _add_release_to_dict(self, name: str, **params):
+        """Add the release data for a specific 'name' to the releases dictionary.
+
+        Parameters
+        ----------
+        name : str
+            The unique name for the release to be added to the dictionary.
+        **params : keyword arguments
+            Parameters to be added for the specific release (e.g., location, volume, etc.).
+        """
+        # Check if the name already exists in the dictionary
+        if name in self.releases:
+            raise ValueError(f"Release name '{name}' already exists in the dictionary.")
+
+        # Extract lat and lon from params
+        lat = params.get("lat")
+        lon = params.get("lon")
+        self._add_grid_indices_to_dict(name=name, lat=lat, lon=lon)
+
+        # Add the parameters to the dictionary under the given name
+        if name not in self.releases:
+            self.releases[name] = {}
+        self.releases[name].update(params)
+
     def plot(self, var_name):
 
         fig, ax = plt.subplots(1, 1, figsize=(7, 4))
@@ -218,7 +292,7 @@ class CDRPipeForcing:
             self.ds[var_name].isel(ncdr=ncdr).plot(
                 ax=ax,
                 linewidth=2,
-                label=self.ds["experiment_name"].isel(ncdr=ncdr).item(),
+                label=self.ds["release_name"].isel(ncdr=ncdr).item(),
             )
 
         if len(self.ds.ncdr) > 0:
@@ -233,20 +307,122 @@ class CDRPipeForcing:
 
         ax.set(title=title, ylabel=ylabel)
 
-    def plot_locations(self, top_view=True):
-        """Plot the locations of CDR releases.
+    def plot_locations(self, top_view=True, releases="all"):
+        """Plot the locations of Carbon Dioxide Removal (CDR) releases.
 
         Parameters
         ----------
         top_view : bool, optional
-            If True (default), plot a top-down (bird's-eye) view.
-            If False, plot side views: longitude vs. depth and latitude vs. depth.
+            If True (default), a top-down (bird's-eye) view is plotted, showing the locations on a 2D map.
+            If False, side views are plotted: longitude vs. depth and latitude vs. depth, showing the
+            vertical profiles of the CDR releases.
+
+        releases : list or str, optional
+            The specific release locations to be plotted. Can either be a list of release names
+            or the string "all" (default), which plots locations for all releases.
+            If top_view is False, only one release is valid, as the lat/lon sections depend on a
+            single release's data for the side views.
+
+        Raises
+        ------
+        ValueError
+            If `top_view` is False and more than one release is provided in `releases`.
+            If any release in `releases` is not found in `self.ds["release_name"]`.
         """
 
-        # if top_view:
-        #    self._plot_locations_top()
-        # else:
-        #    self.plot_locaitons_side()
+        if self.grid is None:
+            raise ValueError(
+                "A grid must be provided when instantiating this class in order to plot data. "
+                "Please pass a valid `Grid` object."
+            )
+
+        # Check that all experiment names are valid if experiments is provided as a list
+        if releases != "all":
+            invalid_releases = [
+                exp for exp in releases if exp not in self.ds["relase_name"]
+            ]
+            if invalid_releases:
+                raise ValueError(
+                    f"The following releases are not valid: {', '.join(invalid_releases)}"
+                )
+
+        # Validate that if top_view is False, only one experiment is passed
+        if not top_view:
+            if releases == "all":
+                raise ValueError(
+                    "When `top_view` is set to False, only one release can be plotted at a time. "
+                    "Please specify a single release name in the `releases` parameter, instead of 'all'."
+                )
+            if isinstance(releases, list) and len(releases) > 1:
+                raise ValueError(
+                    f"When `top_view` is set to False, only one release can be plotted at a time. "
+                    f"You provided: {releases}"
+                )
+        if releases == "all":
+            releases = list(self.releases.keys())
+        elif isinstance(releases, str):
+            releases = [releases]
+
+        if top_view:
+            self._plot_locations_top(releases)
+        else:
+            self._plot_locations_side(releases)
+
+    def _plot_locations_top(self, releases):
+        """Plots the CDR release locations on a bird-eye map projection."""
+
+        field = self.grid.ds.mask_rho
+        lon_deg = self.grid.ds.lon_rho
+        lat_deg = self.grid.ds.lat_rho
+        if self.grid.straddle:
+            lon_deg = xr.where(lon_deg > 180, lon_deg - 360, lon_deg)
+        field = field.assign_coords({"lon": lon_deg, "lat": lat_deg})
+
+        vmax = 3
+        vmin = 0
+        cmap = plt.colormaps.get_cmap("Blues")
+        kwargs = {"vmax": vmax, "vmin": vmin, "cmap": cmap}
+
+        trans = _get_projection(lon_deg, lat_deg)
+
+        fig, ax = plt.subplots(1, 1, figsize=(13, 7), subplot_kw={"projection": trans})
+
+        _plot(field, kwargs=kwargs, ax=ax, c=None, add_colorbar=False)
+
+        proj = ccrs.PlateCarree()
+
+        if len(releases) <= 10:
+            color_map = cm.get_cmap("tab10")
+        elif len(releases) <= 20:
+            color_map = cm.get_cmap("tab20")
+        else:
+            color_map = cm.get_cmap("tab20b")
+        # Create a dictionary of colors
+        colors = {name: color_map(i) for i, name in enumerate(self.releases.keys())}
+
+        for name in releases:
+            eta_index = self.releases[name]["eta_rho"]
+            xi_index = self.releases[name]["xi_rho"]
+
+            # transform coordinates to projected space
+            transformed_lon, transformed_lat = trans.transform_point(
+                self.grid.ds.lon_rho[eta_index, xi_index],
+                self.grid.ds.lat_rho[eta_index, xi_index],
+                proj,
+            )
+
+            ax.plot(
+                transformed_lon,
+                transformed_lat,
+                marker="x",
+                markersize=8,
+                markeredgewidth=2,
+                label=name,
+                color=colors[name],
+            )
+
+        ax.set_title("CDR release locations")
+        ax.legend(loc="center left", bbox_to_anchor=(1.1, 0.5))
 
     def _input_checks(
         self,
@@ -257,9 +433,8 @@ class CDRPipeForcing:
         release_start_time,
         release_end_time,
         times,
-        tracers,
+        tracer_concentrations,
         volume,
-        fill_values,
     ):
         # Check that lat is valid
         if not (-90 <= lat <= 90):
@@ -289,7 +464,7 @@ class CDRPipeForcing:
             if release_start_time >= release_end_time:
                 raise ValueError("release_start_time must be before release_end_time.")
 
-        # Ensure that times is either None (for constant tracers) or a list of datetimes
+        # Ensure that times is either None (for constant tracer concentrations) or a list of datetimes
         if times is not None and not all(isinstance(t, datetime) for t in times):
             raise ValueError(
                 "If 'times' is provided, all entries must be datetime objects."
@@ -305,12 +480,12 @@ class CDRPipeForcing:
                 "Last entry in `times` cannot be after `release_end_time`."
             )
 
-        # Ensure that tracers dictionary is not empty for time-varying forcing
-        # if times is not None and not tracers:
+        # Ensure that tracer concentrations dictionary is not empty for time-varying forcing
+        # if times is not None and not tracer_concentrations:
         #    raise ValueError(
-        #        "The 'tracers' dictionary cannot be empty when 'times' is provided."
+        #        "The 'tracer_concenrations' dictionary cannot be empty when 'times' is provided."
         #    )
-        #    raise ValueError("The 'tracers' dictionary cannot be empty.")
+        #    raise ValueError("The 'tracer_concentrations' dictionary cannot be empty.")
 
         # Check that volume is valid
         if isinstance(volume, float) and volume < 0:
@@ -320,7 +495,7 @@ class CDRPipeForcing:
                 "All entries in 'volume' list must be non-negative numbers."
             )
 
-        # Ensure that time series for 'times', 'volume', and tracers are all the same length
+        # Ensure that time series for 'times', 'volume', and tracer_concentrations are all the same length
         if times is not None:
             num_times = len(times)
 
@@ -330,9 +505,57 @@ class CDRPipeForcing:
                 f"The length of 'volume' ({len(volume)}) does not match the length of 'times' ({num_times})."
             )
 
-        # Check that each time-varying tracer has the same length as 'times'
-        # for key, tracer_values in tracers.items():
+        # Check that each time-varying tracer_concentrations has the same length as 'times'
+        # for key, tracer_values in tracer_concentrations.items():
         #    if isinstance(tracer_values, list) and len(tracer_values) != num_times:
         #        raise ValueError(
         #            f"The length of tracer '{key}' ({len(tracer_values)}) does not match the length of 'times' ({num_times})."
         #        )
+
+    def _add_grid_indices_to_dict(self, name, lat, lon):
+
+        if self.grid:
+            # Adjust longitude based on whether it crosses the International Date Line (straddle case)
+            if self.grid.straddle:
+                lon = xr.where(lon > 180, lon - 360, lon)
+            else:
+                lon = xr.where(lon < 0, lon + 360, lon)
+
+            # maximum dx in grid
+            dx = (
+                np.sqrt((1 / self.grid.ds.pm) ** 2 + (1 / self.grid.ds.pn) ** 2) / 2
+            ).max()
+
+            # Calculate the distance between the grid coordinates and the CDR release location
+            dist = gc_dist(self.grid.ds.lon_rho, self.grid.ds.lat_rho, lon, lat)
+            dist_min = dist.min(dim=["eta_rho", "xi_rho"])
+
+            if (dist_min > dx).all():
+                raise ValueError(
+                    "The specified release location lies outside the model grid domain. "
+                    "Please ensure the provided latitude and longitude fall within the grid boundaries."
+                )
+
+        else:
+            logging.warning(
+                "Grid not provided: cannot verify whether the specified lat/lon/depth location is within the domain or on land. "
+                "Please check manually or provide a grid when instantiating the class."
+            )
+
+        # Find the indices of the closest grid cell
+        indices = np.where(dist == dist_min)
+        eta_rho = indices[0][0]
+        xi_rho = indices[1][0]
+
+        if self.grid.ds.mask_rho[eta_rho, xi_rho].values == 0:
+            raise ValueError(
+                f"The specified release location at grid indices (eta_rho={eta_rho}, xi_rho={xi_rho}) lies on land. "
+                "Please ensure that the provided latitude and longitude fall over ocean in the model grid domain. "
+                "You may need to adjust the release coordinates or provide a grid of finer resolution."
+            )
+
+        if name not in self.releases:
+            self.releases[name] = {}
+
+        self.releases[name]["eta_rho"] = int(eta_rho)
+        self.releases[name]["xi_rho"] = int(xi_rho)

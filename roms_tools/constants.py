0265e7969b2e8e40fd813249c171887dfc460ebb
Nora Loose <nora.loose@gmail.com>
2025-05-22 09:14:52 -0700

    Bump maximum allowable grid size to 25000km

--- roms_tools/constants.py
+++ roms_tools/constants.py
@@ -1,3 +1,3 @@
 R_EARTH = 6371315.0  # Earth radius in meters
-MAXIMUM_GRID_SIZE = 20000  # in km
+MAXIMUM_GRID_SIZE = 25000  # in km
 NUM_TRACERS = 34  # Number of tracers (temperature, salinity, BGC tracers)

03f82df0b57169ffed3b07a3b8ddd4a5885fd6d7
Nora Loose <nora.loose@gmail.com>
2025-05-23 14:51:06 -0700

    Increase upper limit for theta_b to 10

--- roms_tools/constants.py
+++ roms_tools/constants.py
@@ -1,5 +1,5 @@
 R_EARTH = 6371315.0  # Earth radius in meters
 MAXIMUM_GRID_SIZE = 25000  # in km
 UPPER_BOUND_THETA_S = 10  # upper bound for surface vertical stretching parameter
-UPPER_BOUND_THETA_B = 4  # upper bound for bottom vertical stretching parameter
+UPPER_BOUND_THETA_B = 10  # upper bound for bottom vertical stretching parameter
 NUM_TRACERS = 34  # Number of tracers (temperature, salinity, BGC tracers)

bac0a7350e3815e7288305b8be3c8a03be11b924
Scott Eilerman <scott.eilerman@cworthy.org>
2025-05-23 12:59:22 -0600

    Import Grid first

--- roms_tools/__init__.py
+++ roms_tools/__init__.py
@@ -1,4 +1,4 @@
-import logging  # noqa: F811
+import logging
 from importlib.metadata import version as _version
 
 try:
@@ -7,12 +7,12 @@ except ImportError:  # pragma: no cover
     # Local copy or not installed with setuptools
     __version__ = "9999"
 
-
+# grid must be imported first
+from roms_tools.setup.grid import Grid  # noqa: I001, F401
 from roms_tools.analysis.roms_output import ROMSOutput  # noqa: F401
 from roms_tools.setup.boundary_forcing import BoundaryForcing  # noqa: F401
 from roms_tools.setup.cdr_forcing import CDRForcing  # noqa: F401
 from roms_tools.setup.cdr_release import TracerPerturbation, VolumeRelease  # noqa: F401
-from roms_tools.setup.grid import Grid  # noqa: F401
 from roms_tools.setup.initial_conditions import InitialConditions  # noqa: F401
 from roms_tools.setup.nesting import ChildGrid  # noqa: F401
 from roms_tools.setup.river_forcing import RiverForcing  # noqa: F401

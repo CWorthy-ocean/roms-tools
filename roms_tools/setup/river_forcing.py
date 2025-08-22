90e2f6cf0c12ec38ef12956407bc25c6f3fbb8a2
Nora Loose <nora.loose@gmail.com>
2025-08-22 15:57:25 -0600

    Remove extraneous .keys()

--- roms_tools/setup/river_forcing.py
+++ roms_tools/setup/river_forcing.py
@@ -709,7 +709,7 @@ class RiverForcing:
         if self.indices is None:
             valid_river_names = []
         else:
-            valid_river_names = list(self.indices.keys())
+            valid_river_names = list(self.indices)
 
         river_names = _validate_river_names(river_names, valid_river_names)
         if len(valid_river_names) > MAX_DISTINCT_COLORS:
@@ -828,7 +828,7 @@ class RiverForcing:
         if self.indices is None:
             valid_river_names = []
         else:
-            valid_river_names = list(self.indices.keys())
+            valid_river_names = list(self.indices)
 
         river_names = _validate_river_names(river_names, valid_river_names)
         if len(valid_river_names) > MAX_DISTINCT_COLORS:

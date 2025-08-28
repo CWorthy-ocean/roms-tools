89000f13587cca5c52d2477823cd43c8ba07bf45
Nora Loose <nora.loose@gmail.com>
2025-08-28 12:01:56 -0600

    Update docstrings

--- roms_tools/setup/initial_conditions.py
+++ roms_tools/setup/initial_conditions.py
@@ -66,6 +66,7 @@ class InitialConditions:
             - A single Path object.
             - A list of strings or Path objects.
             If omitted, the data will be streamed via the Copernicus Marine Toolkit.
+            Note: streaming is currently not recommended due to performance limitations.
           - "climatology" (bool): Indicates if the data is climatology data. Defaults to False.
 
     bgc_source : Dict[str, Union[str, Path, List[Union[str, Path]]], bool]

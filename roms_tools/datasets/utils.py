b26ef970308e2427960204aa0368bf3d44b16368
Nora Loose <nora.loose@gmail.com>
2025-12-09 11:39:49 -0800

    Test extrapolation to bottom

--- roms_tools/datasets/utils.py
+++ roms_tools/datasets/utils.py
@@ -1,5 +1,33 @@
 import xarray as xr
 
+from roms_tools.fill import one_dim_fill
+
+
+def extrapolate_deepest_to_bottom(ds: xr.Dataset, depth_dim: str) -> xr.Dataset:
+    """Extrapolate the deepest non-NaN values downward along a depth dimension.
+
+    For each variable in the dataset that includes the specified depth dimension,
+    missing values at the bottom are filled by propagating the deepest available
+    data downward.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        Dataset containing variables with a depth dimension.
+    depth_dim : str
+        Name of the depth dimension (e.g., 's_rho') along which to extrapolate.
+
+    Returns
+    -------
+    xr.Dataset
+        Dataset with bottom NaNs filled along the specified depth dimension.
+    """
+    for var_name in ds.data_vars:
+        if depth_dim in ds[var_name].dims:
+            ds[var_name] = one_dim_fill(ds[var_name], depth_dim, direction="forward")
+
+    return ds
+
 
 def convert_to_float64(ds: xr.Dataset) -> xr.Dataset:
     """Convert all data variables in the dataset to float64.
--- roms_tools/tests/test_datasets/test_utils.py
+++ roms_tools/tests/test_datasets/test_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import xarray as xr
 
-from roms_tools.datasets.utils import convert_to_float64
+from roms_tools.datasets.utils import convert_to_float64, extrapolate_deepest_to_bottom
 
 
 def test_convert_to_float64():
@@ -21,3 +21,29 @@ def test_convert_to_float64():
     # Values should be preserved
     xr.testing.assert_equal(out["temp"], ds["temp"].astype("float64"))
     xr.testing.assert_equal(out["mask"], ds["mask"].astype("float64"))
+
+
+def test_extrapolate_deepest_to_bottom():
+    data = np.array(
+        [
+            [1, 2],
+            [3, 4],
+            [5, np.nan],
+            [np.nan, np.nan],
+            [np.nan, np.nan],
+        ]
+    )
+    ds = xr.Dataset({"var": (("s_rho", "x"), data)})
+
+    ds_filled = extrapolate_deepest_to_bottom(ds, "s_rho")
+
+    expected = np.array(
+        [
+            [1, 2],
+            [3, 4],
+            [5, 4],
+            [5, 4],
+            [5, 4],
+        ]
+    )
+    np.testing.assert_array_equal(ds_filled["var"].values, expected)

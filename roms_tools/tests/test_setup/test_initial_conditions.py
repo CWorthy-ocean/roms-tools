1c2eee893e6d4e9fbb5f7fee03c66d39e988d458
Nora Loose <nora.loose@gmail.com>
2025-12-22 15:29:57 -0800

    Fix merge conflict

--- roms_tools/tests/test_setup/test_initial_conditions.py
+++ roms_tools/tests/test_setup/test_initial_conditions.py
@@ -66,7 +66,6 @@ def test_initial_conditions_creation_with_nondefault_glorys_dataset(
     """Test the creation of the InitialConditions object."""
     ic = request.getfixturevalue(ic_fixture)
     assert hasattr(ic.ds, "adjust_depth_for_sea_surface_height")
-    assert ic.ds.attrs["adjust_depth_for_sea_surface_height"] == "False"
     assert isinstance(ic.ds, xr.Dataset)
     assert ic.ds.coords["ocean_time"].attrs["units"] == "seconds"
     expected_vars = {"temp", "salt", "u", "v", "zeta", "ubar", "vbar"}

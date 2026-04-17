7bc051a5aa54acf696b391bf6e8571d545be6583
blsaenz <blsaenz@users.noreply.github.com>
2026-04-17 14:20:35 -0600

    zarr read chunking bug fix, dask documentation improvements (#587)
    zarr read chunking bug fix, dask documentation improvements (typo correction, better explanation of dask and chunks)

--- roms_tools/utils.py
+++ roms_tools/utils.py
@@ -357,6 +357,13 @@ def _load_data_dask(
         )
 
         if read_zarr:
+            # zarr data is always chunked, so we remove any dask chunks except time to let
+            # dask inherit the zarr intrinsic chunking. Otherwise conflicting/non-aligned
+            # chunking causes errors.
+            # TODO: Possibly refactor this into defaults for zarr datasets; perhaps there is
+            # some situation where we want to impose dask chunks on zarr datasets?
+            chunks = {dim_names["time"]: 1}
+
             return xr.open_zarr(
                 filenames[0],
                 decode_times=decode_times,

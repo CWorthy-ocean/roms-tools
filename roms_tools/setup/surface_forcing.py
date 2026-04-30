c48bc6b496a22e541b8cb58e291279988c33ddca
SamM <smaticka@gmail.com>
2026-04-30 18:41:50 -0400

    move restoring_forces check to input_checks

--- roms_tools/setup/surface_forcing.py
+++ roms_tools/setup/surface_forcing.py
@@ -186,13 +186,9 @@ class SurfaceForcing:
         elif self.type == "restoring":
             opt_file = "cppdefs.opt"
             cppdefs_flags = set()
-            if self.restoring_forces is not None:
-                for var in self.restoring_forces:
-                    if var == "sss":
-                        cppdefs_flags.add("SFLX_CORR")
-            else:
-                self.restoring_forces = ["sss"]
-                cppdefs_flags.add("SFLX_CORR")
+            for var in self.restoring_forces:
+                if var == "sss":
+                    cppdefs_flags.add("SFLX_CORR")
         grid_desc = "grid coarsened by factor 2" if use_coarse_grid else "fine grid"
         interp_flag = 1 if use_coarse_grid else 0
 
@@ -335,8 +331,12 @@ class SurfaceForcing:
             )
 
         # Check if restoring variables are accepted
-        valid_vars = ["sss"]
-        if self.restoring_forces:
+        if self.type == "restoring":
+            if self.restoring_forces is None:
+                raise ValueError(
+                    "When type='restoring', `restoring_forces` must be defined."
+                )
+            valid_vars = ["sss"]
             for var in self.restoring_forces:
                 if var not in valid_vars:
                     raise ValueError(
